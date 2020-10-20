2c84f401a9b5985400e6fd1cae4b2b836d12180d
Darryl Hodgins <darrylh@darrylh.ca>
2020-10-20 03:40:15 -0700

    feat: update run script to avoid upgrade warning popup

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-/usr/bin/chromium-browser --app=http://localhost \
+/usr/bin/chromium-browser --app=http://localhost/index.html \
 	--kiosk \
 	--noerrdialogs \
 	--disable-session-crashed-bubble \
-	--disable-infobars
+	--disable-infobars \
+	--check-for-update-interval=1 \
+	--simulate-critical-update
 
