bce97c0728ddf8266593bf4dc047371db39ad077
Darryl Hodgins <darrylh@darrylh.ca>
2020-10-20 04:01:32 -0700

    feat: add json to tft backlight

--- html/pi/tft-backlight.php
+++ html/pi/tft-backlight.php
@@ -1,13 +1,23 @@
 <?php
-echo "foo";
+
 $power = $_GET["power"];
+$success = "false";
+$powerVal = "null";
 
 shell_exec("/usr/bin/gpio -g mode 18 out");
 
 if ($power == 'on') {
 	shell_exec("/usr/bin/gpio -g write 18 1");
+	$success = "true";
+	$powerVal = "true";
 } else if ($power == 'off') {
 	shell_exec("/usr/bin/gpio -g write 18 0");
+	$success = "true";
+	$powerVal = "false";
 }
 
 ?>
+{
+	"success": <?php echo $success ?>,
+	"power": <?php echo $powerVal ?>
+}
