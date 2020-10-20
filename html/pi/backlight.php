74f8dba960272bf1659202624638441519a777a4
Darryl Hodgins <darrylh@darrylh.ca>
2020-10-20 03:50:19 -0700

    feat: add json response to backlight

--- html/pi/backlight.php
+++ html/pi/backlight.php
@@ -1,11 +1,19 @@
 <?php
 
 $power = $_GET["power"];
+$powerVal = "null";
 
 if ($power == 'on') {
 	file_put_contents("/sys/class/backlight/rpi_backlight/bl_power", "0");
+	$powerVal = "true";
 } else if ($power == 'off') {
 	file_put_contents("/sys/class/backlight/rpi_backlight/bl_power", "1");
+	$powerVal = "false";
 }
 
 ?>
+{
+	"success": true,
+	"power": <?php echo $powerVal ?>
+}
+
